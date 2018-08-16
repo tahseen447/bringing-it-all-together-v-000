d8815854965ad71b3cd80f53caed0c4eda52220e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-16 23:32:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,8 +33,8 @@ class Dog
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() from dogs")[0][0]
-      self
     end
+    self
     end
 
     def self.create(name:, breed:)
