cc19489608ccc9a2c4f0def88025a78d94686eb7
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-16 23:51:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,6 +59,7 @@ class Dog
       dog = DB[:conn].execute("select * from dogs where name=? and breed =?", name, breed)
 
       if !dog.empty?
+        dog_date = dog[0]
         new_dog = Dog.new(id:dog[0], name:dog[1], breed:dog[2] )
       else
         new_dog = self.create(name:name, breed:breed)
