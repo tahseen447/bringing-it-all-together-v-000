46884b332133fe8b60563399d648a447af1b3382
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-16 23:47:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,6 +67,10 @@ class Dog
     end
 
     def self.find_by_name(name)
+      sql = "SELECT * from dogs where id=? limit 1"
+      DB[:conn].execute(sql, id).map do |row|
+        self.create(name:row[1], breed:row[2] )
+      end.first
     end
 
     def self.new_from_db(row)
